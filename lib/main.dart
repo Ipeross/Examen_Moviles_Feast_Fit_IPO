d0ebd2b00ad0a7c47bb86b1b8d3616aab6fb7e35
Alejandro Castellon <alexcastellon077@gmail.com>
2025-02-07 11:16:04 +0100

    Change main to implement the dark and light mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,33 @@
-import 'package:feast_fit/screens/screens.dart';
+import 'package:feast_fit/screens/login_screen.dart';
+import 'package:feast_fit/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      title: 'Flutter Theme Switch',
+      themeMode: themeProvider.themeMode,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: const LoginScreen(),
     );
   }
 }
