a6b3ca2697fd20f03c2885a2649cdd47c307db9b
CASTELLON13 <alexcastellon077@gmail.com>
2025-02-07 18:33:15 +0100

    Add styles in AboutUsScreen

--- lib/screens/about_us_screen.dart
+++ lib/screens/about_us_screen.dart
@@ -8,32 +8,50 @@ class AboutUsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Información'),
+        foregroundColor: Colors.white,
+        backgroundColor: const Color.fromARGB(255, 124, 113, 99),
       ),
       body: ListView(
         padding: const EdgeInsets.all(16.0),
         children: const [
           Text(
             'Sobre Nosotros',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 104, 85, 56),
+            ),
           ),
           SizedBox(height: 10),
           Text(
             'Aquí puedes escribir información sobre nosotros. '
-            'Este es el apartado donde podríamos detallar quiénes son las personas detrás de la empresa, '
+            'Este es el apartado donde puedes detallar quiénes son las personas detrás de la empresa, '
             'sus valores, su misión y cualquier otra información relevante.',
-            style: TextStyle(fontSize: 16),
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.black87,
+              height: 1.5,
+            ),
           ),
           SizedBox(height: 20),
           Text(
             'Sobre la Empresa',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 104, 85, 56),
+            ),
           ),
           SizedBox(height: 10),
           Text(
             'Aquí puedes escribir información sobre la empresa. '
             'Este es el apartado donde podríamos detallar la historia de la empresa, '
             'sus productos o servicios, su visión y cualquier otra información relevante.',
-            style: TextStyle(fontSize: 16),
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.black87,
+              height: 1.5,
+            ),
           ),
         ],
       ),
