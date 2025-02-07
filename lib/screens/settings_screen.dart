c9fd67bd9427067cd5525a94d271b07ac1ef5efd
Alejandro Castellon <alexcastellon077@gmail.com>
2025-02-07 12:10:34 +0100

    New style and implementation the ThemeMode

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,22 +1,33 @@
-import 'package:feast_fit/screens/screens.dart';
+import 'package:feast_fit/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
-
-
+import 'package:provider/provider.dart';
+import 'profile_screen.dart'; // Asegúrate de que la ruta sea correcta
+import 'about_us_screen.dart'; // Asegúrate de que la ruta sea correcta
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+        final themeProvider = Provider.of<ThemeProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Configuración'),
+        actions: [
+          IconButton(
+            icon: Icon(themeProvider.themeMode == ThemeMode.dark ? Icons.light_mode : Icons.dark_mode),
+            onPressed: () {
+              themeProvider.toggleTheme();
+            },
+          ),
+        ],
       ),
       body: ListView(
-        padding: const EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(16.0),
         children: [
           ListTile(
-            leading: const Icon(Icons.person),
+            leading: const Icon(Icons.person, color: Colors.blueAccent),
             title: const Text('Perfil'),
             onTap: () {
               Navigator.push(
@@ -34,8 +45,9 @@ class SettingsScreen extends StatelessWidget {
               );
             },
           ),
+          const Divider(),
           ListTile(
-            leading: const Icon(Icons.info),
+            leading: const Icon(Icons.info, color: Colors.blueAccent),
             title: const Text('Acerca de Nosotros'),
             onTap: () {
               Navigator.push(
