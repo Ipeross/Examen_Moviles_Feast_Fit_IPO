c033a7d3612e17f73b92c201cf4ab5e5eaa8007d
Alejandro Castellon <alexcastellon077@gmail.com>
2025-02-07 09:41:10 +0100

    Finish  all the fl-screen

---
+++ lib/screens/food_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FoodScreen extends StatelessWidget {
+   
+  const FoodScreen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+         child: Text('FoodScreen'),
+      ),
+    );
+  }
+}
