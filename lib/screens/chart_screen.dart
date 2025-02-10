e22a606f2bcf0c965f6fa9534903563942f97f12
Alejandro Castellon <alexcastellon077@gmail.com>
2025-02-10 09:36:13 +0100

    Add appBar on FoodScreen and ChartScreen

--- lib/screens/chart_screen.dart
+++ lib/screens/chart_screen.dart
@@ -1,12 +1,14 @@
+import 'package:feast_fit/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class ChartScreen extends StatelessWidget {
    
-  const ChartScreen({Key? key}) : super(key: key);
+  const ChartScreen({super.key});
   
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      appBar: CustomAppBar2(title: 'Gráfico'),
       body: Center(
          child: Text('ChartScreen'),
       ),
--- lib/screens/food_screen.dart
+++ lib/screens/food_screen.dart
@@ -1,12 +1,14 @@
+import 'package:feast_fit/widgets/custom_appbar_2_elements.dart';
 import 'package:flutter/material.dart';
 
 class FoodScreen extends StatelessWidget {
    
-  const FoodScreen({Key? key}) : super(key: key);
+  const FoodScreen({super.key});
   
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      appBar: CustomAppBar2(title: 'Dieta'),
       body: Center(
          child: Text('FoodScreen'),
       ),
