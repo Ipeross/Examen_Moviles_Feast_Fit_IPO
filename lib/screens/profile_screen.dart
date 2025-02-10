c23828f84719ab23f75b3fcb930729a250b32800
Alejandro Castellon <alexcastellon077@gmail.com>
2025-02-10 09:24:02 +0100

    Using widget on ProfileScreen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,65 +1,15 @@
-import 'package:feast_fit/screens/screens.dart';
-import 'package:feast_fit/theme/theme_provider.dart';
+import 'package:feast_fit/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key, required Map<String, String> userData});
 
   @override
   Widget build(BuildContext context) {
-    final themeProvider = Provider.of<ThemeProvider>(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text('Profile'),
-        actions: [
-          PopupMenuButton<String>(
-            onSelected: (value) {
-              if (value == 'settings') {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SettingsScreen()),
-                );
-              }else if (value == 'home') {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const HomeScreen()),
-                );
-              } else if (value == 'toggle_theme') {
-                themeProvider.toggleTheme();
-              }
-            },
-            itemBuilder: (BuildContext context) {
-              return [
-                PopupMenuItem<String>(
-                  value: 'toggle_theme',
-                  child: ListTile(
-                    leading: Icon(themeProvider.themeMode == ThemeMode.dark ? Icons.light_mode : Icons.dark_mode),
-                    title: Text(themeProvider.themeMode == ThemeMode.dark ? 'Modo Claro' : 'Modo Oscuro'),
-                  ),
-                ),
-                const PopupMenuItem<String>(
-                  value: 'settings',
-                  child: ListTile(
-                    leading: Icon(Icons.settings),
-                    title: Text('Ajustes'),
-                  ),
-                ),
-                const PopupMenuItem<String>(
-                  value: 'home',
-                  child: ListTile(
-                    leading: Icon(Icons.home),
-                    title: Text('Home'),
-                  ),
-                ),
-              ];
-            },
-          ),
-        ],
-      ),
-      body: const Center(child: Text('Hola Mundo')),
+    return const Scaffold(
+      appBar: CustomAppBar(title: 'Perfil'),
+      body: Center(child: Text('Hola Mundo')),
     );
   }
 }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,2 +1,3 @@
 export 'package:feast_fit/widgets/settings_list.dart';
 export 'package:feast_fit/widgets/info_section.dart';
+export 'package:feast_fit/widgets/custom_appbar.dart';
