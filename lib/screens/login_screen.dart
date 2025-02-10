07e508fcb7cf6654e89df65d24149e9fac3f0d77
Alejandro Castellon <alexcastellon077@gmail.com>
2025-02-10 10:21:55 +0100

    Implementation widget on LoginScreen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:feast_fit/screens/forgot_password_screen.dart';
 import 'package:feast_fit/screens/screens.dart';
+import 'package:feast_fit/widgets/widgets.dart';
+
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -38,27 +40,7 @@ class _LoginScreenState extends State<LoginScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AnimatedBuilder(
-        animation: _animationController,
-        builder: (context, child) {
-          return Container(
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-              begin: Alignment(
-                0.0, // Fijo en x
-                -1.0 + (1 * _animationController.value), // Varía de -1 a 1 en y
-              ),
-              end: Alignment(
-                0.0, // Fijo en x
-                1.5 + (1 * _animationController.value), // Varía de 0 a 2 en y
-              ),
-              colors: const [
-                Color.fromARGB(255, 78, 61, 61),
-                Color.fromARGB(255, 105, 82, 64),
-                Color.fromARGB(255, 95, 75, 67),
-                Color.fromARGB(255, 83, 70, 70),
-              ],
-            )),
+      body: AnimatedGradientBackground(
             child: SafeArea(
               child: Center(
                 child: SingleChildScrollView(
@@ -198,10 +180,8 @@ class _LoginScreenState extends State<LoginScreen>
                 ),
               ),
             ),
+          )
           );
-        },
-      ),
-    );
   }
 
   @override
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -2,3 +2,4 @@ export 'package:feast_fit/widgets/settings_list.dart';
 export 'package:feast_fit/widgets/info_section.dart';
 export 'package:feast_fit/widgets/custom_appbar_3_elements.dart';
 export 'package:feast_fit/widgets/custom_appbar_2_elements.dart';
+export 'package:feast_fit/widgets/animated_gradiant_background.dart';
