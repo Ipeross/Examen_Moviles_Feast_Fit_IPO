e29760ab660c460e889c63f5bf7ce7677081d78f
Alejandro Castellon <alexcastellon077@gmail.com>
2025-02-10 09:33:23 +0100

    Using widget on HomeScreen

--- lib/screens/about_us_screen.dart
+++ lib/screens/about_us_screen.dart
@@ -2,7 +2,7 @@ import 'package:feast_fit/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class AboutUsScreen extends StatelessWidget {
-  const AboutUsScreen({Key? key}) : super(key: key);
+  const AboutUsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,53 +1,16 @@
-import 'package:feast_fit/theme/theme_provider.dart';
+import 'package:feast_fit/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'settings_screen.dart'; // Asegúrate de importar SettingsScreen
+ // Asegúrate de importar SettingsScreen
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final themeProvider = Provider.of<ThemeProvider>(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text('Home'),
-        actions: [
-          PopupMenuButton<String>(
-            onSelected: (value) {
-              if (value == 'settings') {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SettingsScreen()),
-                );
-              } else if (value == 'toggle_theme') {
-                themeProvider.toggleTheme();
-              }
-            },
-            itemBuilder: (BuildContext context) {
-              return [
-                PopupMenuItem<String>(
-                  value: 'toggle_theme',
-                  child: ListTile(
-                    leading: Icon(themeProvider.themeMode == ThemeMode.dark ? Icons.light_mode : Icons.dark_mode),
-                    title: Text(themeProvider.themeMode == ThemeMode.dark ? 'Modo Claro' : 'Modo Oscuro'),
-                  ),
-                ),
-                const PopupMenuItem<String>(
-                  value: 'settings',
-                  child: ListTile(
-                    leading: Icon(Icons.settings),
-                    title: Text('Ajustes'),
-                  ),
-                ),
-              ];
-            },
-          ),
-        ],
-      ),
-      body: const Center(child: Text('Hola Mundo')),
+    return const Scaffold(
+      appBar: CustomAppBar2(title: 'Inicio'),
+      body: Center(child: Text('Hola Mundo')),
     );
   }
 }
