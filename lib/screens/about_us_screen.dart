f59e0eceff458ad1a8b7eabb29889848485ee244
Alejandro Castellon <alexcastellon077@gmail.com>
2025-02-10 08:49:59 +0100

    Edit menu in AboutUsScreen

--- lib/screens/about_us_screen.dart
+++ lib/screens/about_us_screen.dart
@@ -1,15 +1,62 @@
+import 'package:feast_fit/screens/screens.dart';
+import 'package:feast_fit/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class AboutUsScreen extends StatelessWidget {
   const AboutUsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Información'),
-        foregroundColor: Colors.white,
-        backgroundColor: const Color.fromARGB(255, 124, 113, 99),
+        title: const Text('Profile'),
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              if (value == 'settings') {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SettingsScreen()),
+                );
+              }else if (value == 'home') {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomeScreen()),
+                );
+              } else if (value == 'toggle_theme') {
+                themeProvider.toggleTheme();
+              }
+            },
+            itemBuilder: (BuildContext context) {
+              return [
+                PopupMenuItem<String>(
+                  value: 'toggle_theme',
+                  child: ListTile(
+                    leading: Icon(themeProvider.themeMode == ThemeMode.dark ? Icons.light_mode : Icons.dark_mode),
+                    title: Text(themeProvider.themeMode == ThemeMode.dark ? 'Modo Claro' : 'Modo Oscuro'),
+                  ),
+                ),
+                const PopupMenuItem<String>(
+                  value: 'settings',
+                  child: ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Text('Ajustes'),
+                  ),
+                ),
+                const PopupMenuItem<String>(
+                  value: 'home',
+                  child: ListTile(
+                    leading: Icon(Icons.home),
+                    title: Text('Home'),
+                  ),
+                ),
+              ];
+            },
+          ),
+        ],
       ),
       body: ListView(
         padding: const EdgeInsets.all(16.0),
