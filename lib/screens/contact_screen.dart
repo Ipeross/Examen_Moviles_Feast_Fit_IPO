a11ef2ee3bee2088e4ad0039c046a4268e83ad6e
Alejandro Castellon <alexcastellon077@gmail.com>
2025-02-18 13:59:16 +0100

    Validation in ContactScreen implements

--- lib/screens/contact_screen.dart
+++ lib/screens/contact_screen.dart
@@ -3,8 +3,18 @@ import 'package:feast_fit/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class ContactScreen extends StatelessWidget {
+class ContactScreen extends StatefulWidget {
   const ContactScreen({Key? key}) : super(key: key);
+
+  @override
+  _ContactScreenState createState() => _ContactScreenState();
+}
+
+class _ContactScreenState extends State<ContactScreen> {
+  final _formKey = GlobalKey<FormState>();  // Key para manejar el estado del formulario
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _messageController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -14,99 +24,127 @@ class ContactScreen extends StatelessWidget {
       appBar: const CustomAppBar3(title: '¿Necesitas ayuda?'),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Estamos aquí para ayudarte. Completa el formulario o utiliza nuestra información de contacto.',
-              style: TextStyle(
-                fontSize: 16,
-              ),
-            ),
-            const SizedBox(height: 24),
-
-            // Formulario de contacto
-            const Text(
-              'Nombre',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 71, 52, 45),
-              ),
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Ingresa tu nombre',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(color: Color.fromARGB(255, 71, 52, 45)),
+        child: Form(  // Aquí encapsulamos todo el contenido en un Form
+          key: _formKey,  // El formulario está asociado a esta clave
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Estamos aquí para ayudarte. Completa el formulario o utiliza nuestra información de contacto.',
+                style: TextStyle(
+                  fontSize: 16,
                 ),
               ),
-            ),
-            const SizedBox(height: 16),
+              const SizedBox(height: 24),
 
-            const Text(
-              'Correo electrónico',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 71, 52, 45)
-              ),
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Ingresa tu correo electrónico',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(color: Color.fromARGB(255, 71, 52, 45)),
+              // Formulario de contacto
+              const Text(
+                'Nombre',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 71, 52, 45),
                 ),
               ),
-            ),
-            const SizedBox(height: 16),
-
-            const Text(
-              'Mensaje',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 71, 52, 45)
+              const SizedBox(height: 8),
+              TextFormField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                  hintText: 'Ingresa tu nombre',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(color: Color.fromARGB(255, 71, 52, 45)),
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor ingresa tu nombre';
+                  }
+                  return null;
+                },
               ),
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              maxLines: 5,
-              decoration: InputDecoration(
-                hintText: 'Escribe tu mensaje aquí',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(color: Color.fromARGB(255, 71, 52, 45)),
+              const SizedBox(height: 16),
+
+              const Text(
+                'Correo electrónico',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 71, 52, 45),
                 ),
               ),
-            ),
-            const SizedBox(height: 16),
+              const SizedBox(height: 8),
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  hintText: 'Ingresa tu correo electrónico',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(color: Color.fromARGB(255, 71, 52, 45)),
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor ingresa tu correo electrónico';
+                  } else if (!RegExp(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$").hasMatch(value)) {
+                    return 'Por favor ingresa un correo electrónico válido';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
 
-            // Botón de enviar
-            ElevatedButton(
-              onPressed: () {
-                // Acción para enviar el formulario (por ahora no tiene funcionalidad)
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Formulario enviado')),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: themeProvider.themeMode == ThemeMode.dark
-                  ? const Color.fromARGB(255, 182, 156, 99)
-                  : const Color.fromARGB(255, 117, 95, 62),
-                padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
+              const Text(
+                'Mensaje',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 71, 52, 45),
                 ),
               ),
-              child: const Text('Enviar', style: TextStyle(fontSize: 16)),
-            ),
-          ],
+              const SizedBox(height: 8),
+              TextFormField(
+                controller: _messageController,
+                maxLines: 5,
+                decoration: InputDecoration(
+                  hintText: 'Escribe tu mensaje aquí',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(color: Color.fromARGB(255, 71, 52, 45)),
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor ingresa tu mensaje';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+
+              // Botón de enviar
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState?.validate() ?? false) {
+                    // Si el formulario es válido, se ejecuta la acción
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Formulario enviado')),
+                    );
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: themeProvider.themeMode == ThemeMode.dark
+                    ? const Color.fromARGB(255, 182, 156, 99)
+                    : const Color.fromARGB(255, 117, 95, 62),
+                  padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: const Text('Enviar', style: TextStyle(fontSize: 16, color: Colors.white)),
+              ),
+            ],
+          ),
         ),
       ),
     );
