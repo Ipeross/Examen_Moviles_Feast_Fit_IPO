a34b6fc3fed6a00964faa661698f25e6487c221b
Alejandro Castellon <alexcastellon077@gmail.com>
2025-02-18 10:19:12 +0100

    Start the form on ContactScreen

--- lib/screens/contact_screen.dart
+++ lib/screens/contact_screen.dart
@@ -10,17 +10,79 @@ class ContactScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
 
-    return const Scaffold(
-      appBar: CustomAppBar3(title: '¿Necesitas ayuda?'),
+    return Scaffold(
+      appBar: const CustomAppBar3(title: '¿Necesitas ayuda?'),
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
+            const Text(
               'Estamos aquí para ayudarte. Completa el formulario o utiliza nuestra información de contacto.',
               style: TextStyle(
                 fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 24),
+
+            // Formulario de contacto
+            const Text(
+              'Nombre',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 71, 52, 45),
+              ),
+            ),
+            const SizedBox(height: 8),
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Ingresa tu nombre',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: const BorderSide(color: Color.fromARGB(255, 71, 52, 45)),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+
+            const Text(
+              'Correo electrónico',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 71, 52, 45)
+              ),
+            ),
+            const SizedBox(height: 8),
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Ingresa tu correo electrónico',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: const BorderSide(color: Color.fromARGB(255, 71, 52, 45)),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+
+            const Text(
+              'Mensaje',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 71, 52, 45)
+              ),
+            ),
+            const SizedBox(height: 8),
+            TextFormField(
+              maxLines: 5,
+              decoration: InputDecoration(
+                hintText: 'Escribe tu mensaje aquí',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: const BorderSide(color: Color.fromARGB(255, 71, 52, 45)),
+                ),
               ),
             ),
           ],
