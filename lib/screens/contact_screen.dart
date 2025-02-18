75d59ceb22ef3adcc488a2823c669d8b7387634c
Alejandro Castellon <alexcastellon077@gmail.com>
2025-02-18 10:12:35 +0100

    Implementation appBar on ContactScreen

--- lib/screens/contact_screen.dart
+++ lib/screens/contact_screen.dart
@@ -1,14 +1,22 @@
+import 'package:feast_fit/theme/theme_provider.dart';
+import 'package:feast_fit/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ContactScreen extends StatelessWidget {
-   
   const ContactScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    return Scaffold(
+      appBar: const CustomAppBar3(title: 'Contacto'),
       body: Center(
-         child: Text('ContactScreen'),
+        child: Text(
+          'Contenido de la pantalla de contacto',
+          style: TextStyle(color: themeProvider.themeMode == ThemeMode.dark ? Colors.white : Colors.black),
+        ),
       ),
     );
   }
