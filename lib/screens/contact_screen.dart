34519a4452feca47b55ce5fbea7e7597e8d84a33
Alejandro Castellon <alexcastellon077@gmail.com>
2025-02-18 10:14:53 +0100

    Add an introduccion to the ContactScreen

--- lib/screens/contact_screen.dart
+++ lib/screens/contact_screen.dart
@@ -10,12 +10,20 @@ class ContactScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
 
-    return Scaffold(
-      appBar: const CustomAppBar3(title: 'Contacto'),
-      body: Center(
-        child: Text(
-          'Contenido de la pantalla de contacto',
-          style: TextStyle(color: themeProvider.themeMode == ThemeMode.dark ? Colors.white : Colors.black),
+    return const Scaffold(
+      appBar: CustomAppBar3(title: '¿Necesitas ayuda?'),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Estamos aquí para ayudarte. Completa el formulario o utiliza nuestra información de contacto.',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ],
         ),
       ),
     );
