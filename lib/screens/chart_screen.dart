fe144b1a73383cf2382fac5125cf3d99a98f208d
Alejandro Castellon <alexcastellon077@gmail.com>
2025-02-21 09:04:30 +0100

    Use a ElevatedButton to change the Chart

--- lib/screens/chart_screen.dart
+++ lib/screens/chart_screen.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 
-class ChartScreen extends StatelessWidget {
+class ChartScreen extends StatefulWidget {
+  @override
+  _ChartScreenState createState() => _ChartScreenState();
+}
+
+class _ChartScreenState extends State<ChartScreen> {
+  bool isBarChart = true;
+
+  void toggleChartType() {
+    setState(() {
+      isBarChart = !isBarChart;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,59 +24,58 @@ class ChartScreen extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('Gráfico de Barras', style: TextStyle(fontSize: 20)),
           SizedBox(
             width: 300,
             height: 300,
-            child: BarChart(
-              BarChartData(
-                barGroups: [
-                  BarChartGroupData(
-                    x: 0,
-                    barRods: [
-                      BarChartRodData(y: 12, colors: [Colors.blue]),
-                    ],
+            child: isBarChart
+                ? BarChart(
+                    BarChartData(
+                      barGroups: [
+                        BarChartGroupData(
+                          x: 0,
+                          barRods: [
+                            BarChartRodData(y: 12, colors: [Colors.blue]),
+                          ],
+                        ),
+                        BarChartGroupData(
+                          x: 1,
+                          barRods: [
+                            BarChartRodData(y: 8, colors: [Colors.red]),
+                          ],
+                        ),
+                        BarChartGroupData(
+                          x: 2,
+                          barRods: [
+                            BarChartRodData(y: 15, colors: [Colors.green]),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                : LineChart(
+                    LineChartData(
+                      lineBarsData: [
+                        LineChartBarData(
+                          spots: [
+                            const FlSpot(0, 12),
+                            const FlSpot(1, 8),
+                            const FlSpot(2, 15),
+                          ],
+                          isCurved: true,
+                          colors: [Colors.blue],
+                          barWidth: 4,
+                          isStrokeCapRound: true,
+                          dotData: FlDotData(show: false),
+                          belowBarData: BarAreaData(show: false),
+                        ),
+                      ],
+                    ),
                   ),
-                  BarChartGroupData(
-                    x: 1,
-                    barRods: [
-                      BarChartRodData(y: 8, colors: [Colors.red]),
-                    ],
-                  ),
-                  BarChartGroupData(
-                    x: 2,
-                    barRods: [
-                      BarChartRodData(y: 15, colors: [Colors.green]),
-                    ],
-                  ),
-                ],
-              ),
-            ),
           ),
           const SizedBox(height: 20),
-          const Text('Gráfico de Líneas', style: TextStyle(fontSize: 20)),
-          SizedBox(
-            width: 300,
-            height: 300,
-            child: LineChart(
-              LineChartData(
-                lineBarsData: [
-                  LineChartBarData(
-                    spots: [
-                      const FlSpot(0, 12),
-                      const FlSpot(1, 8),
-                      const FlSpot(2, 15),
-                    ],
-                    isCurved: true,
-                    colors: [Colors.blue],
-                    barWidth: 4,
-                    isStrokeCapRound: true,
-                    dotData: FlDotData(show: false),
-                    belowBarData: BarAreaData(show: false),
-                  ),
-                ],
-              ),
-            ),
+          ElevatedButton(
+            onPressed: toggleChartType,
+            child: Text(isBarChart ? 'Cambiar a Gráfico de Líneas' : 'Cambiar a Gráfico de Barras'),
           ),
         ],
       ),
